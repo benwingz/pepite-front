60f15433828609e58944c9785eb325fb5decdeea
ben <b.roullet@skilvioo.com>
2017-04-05 17:06:04 +0200

    :tada: Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { DropdownValue } from './common/dropdown/dropdown.component';
+
+import { SessionService } from './service/session.service';
+import { UserService } from './service/user.service';
+
+import { User } from './models/user.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  currentUser: User = null;
+  title = 'Pépite';
+  private dropdownValues: DropdownValue [];
+  private sidebarShown: Boolean = false;
+
+  constructor(
+    private router: Router,
+    private sessionService: SessionService,
+    private userService: UserService
+  ) {
+    this.dropdownValues = [
+      new DropdownValue('auto_eval', 'Auto-evaluation', true),
+      new DropdownValue('validation', 'Validation'),
+      new DropdownValue('certification_deliver', 'Délivrer un certificat'),
+      new DropdownValue('certification_download', 'Télécharger un certificat'),
+    ];
+  }
+
+  select(value: string) {}
+
+  toggleSidebar(): void {}
+
+  ngOnInit(): void {
+    this.sessionService.generateToken({token : this.sessionService.getStoredToken()})
+      .then((token) => {
+        if(token) {
+          this.retriveUserProfile();
+          this.router.navigate(['home']);
+        } else {
+          //go to login page
+        }
+      });
+  }
+
+  retriveUserProfile(): void{
+    this.userService.getCurrentUser().subscribe(user => {
+      this.currentUser = new User(user._id, user.lastname, user.firstname, user.password, user.salt);
+    });
+  }
+
+  getRole(): void{}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { Http, HttpModule, RequestOptions } from '@angular/http';
+import { AuthHttp, AuthConfig } from 'angular2-jwt';
+
+import { AppRoutingModule } from './app.routing.module';
 
 import { AppComponent } from './app.component';
 
+import { DropdownComponent } from './common/dropdown/dropdown.component';
+import { BackToTopComponent } from './common/back-to-top/back-to-top.component';
+
+import { SessionService } from './service/session.service';
+import { UserService } from './service/user.service';
+import { ReferenceService } from './service/reference.service';
+import { GradeService } from './service/grade.service';
+
+import { EvaluatedGradePipe } from './pipes/evaluatedgrade.pipe'
+
+import { HomeComponent } from './pages/home/home.component';
+import { PhaseComponent } from './pages/phase/phase.component';
+
+export function authHttpServiceFactory(http: Http, options: RequestOptions) {
+  return new AuthHttp(new AuthConfig({
+    tokenName: 'token',
+		tokenGetter: (() => sessionStorage.getItem('token'))
+  }), http, options);
+}
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownComponent,
+    BackToTopComponent,
+    HomeComponent,
+    EvaluatedGradePipe,
+    PhaseComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [{
+      provide: AuthHttp,
+      useFactory: authHttpServiceFactory,
+      deps: [Http, RequestOptions]
+    },
+    SessionService,
+    UserService,
+    ReferenceService,
+    GradeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
