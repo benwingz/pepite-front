82c581dd0275f4d8bdc604247cb9701d13dead8a
ben <b.roullet@skilvioo.com>
2017-04-06 11:58:56 +0200

    :construction: Refacto auth service

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
-import {Observable} from 'rxjs/Rx';
-
-import { UserService } from './user.service';
+import { Observable } from 'rxjs/Rx';
+import { AuthHttp, AuthConfig } from 'angular2-jwt';
+import { User } from '../models/user.model';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
-export class SessionService {
+export class AuthService {
 
-  token: string;
+  public token: string;
+  public currentUser: User;
 
   constructor(
     private http: Http,
-    private userService: UserService
+    private authHttp: AuthHttp
   ) {
   };
 
@@ -24,7 +25,7 @@ export class SessionService {
         .subscribe((responseJson) => {
           this.storeNewtoken(responseJson.token);
           console.log('User Id:', responseJson.user_id);
-          this.userService.storeUserId(responseJson.user_id);
+          this.storeUserId(responseJson.user_id);
           resolve(responseJson.token)
         });
     });
@@ -43,6 +44,34 @@ export class SessionService {
 
   getStoredToken():string {
     return sessionStorage.getItem('token');
+  }
+
+  getCurrentUser(): Observable<User> {
+    return this.authHttp.get('http://localhost:8080/api/user/' + this.getUserIdFromLocalStorage())
+      .map(user => this.storeUser(user.json()));
+  }
+
+  getUserIdFromLocalStorage(): string {
+    return sessionStorage.getItem('user_id');
+  }
+
+  storeUserId(id) {
+    sessionStorage.setItem('user_id', id);
+  }
+
+  storeUser(user) {
+    this.currentUser = new User (user._id, user.lastname, user.firstname, user.password, user.salt);
+    return user;
+  }
+
+  getUser(): any {
+    return new Promise<User>(function(resolve, reject) {
+      if (this.currentUser) {
+        resolve(this.currentUser);
+      } else {
+        reject(false);
+      }
+    });
   }
 
 }
