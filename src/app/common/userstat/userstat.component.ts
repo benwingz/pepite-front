937531b19787b1e52c5d2b9826adde99a559cfe9
virgil <v.roger@skilvioo.com>
2017-04-24 16:26:09 +0200

    :sparkles: Create users stats

---
+++ src/app/common/userstat/userstat.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { ReferenceService } from '../../service/reference.service';
+import { GradeService } from '../../service/grade.service';
+
+import { Category } from '../../models/category.model';
+import { Grade } from '../../models/grade.model';
+
+@Component({
+  selector: 'app-userstat',
+  templateUrl: './userstat.component.html',
+  styleUrls: ['./userstat.component.scss'],
+})
+export class UserstatComponent implements OnInit {
+
+  @Input()
+  user: string;
+  @Input()
+  hideCategories: boolean = false;
+  categories: Category[];
+  grades: Grade[];
+
+  constructor(
+    private referenceService: ReferenceService,
+    private gradeService: GradeService
+  ) {}
+
+  ngOnInit(): void {
+    this.referenceService.getCategories().subscribe((categories) => {
+      this.categories = categories;
+    });
+    this.gradeService.getGrades(this.user).subscribe((grades) => {
+      this.grades = grades;
+    });
+  }
+
+}
