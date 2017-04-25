6958c34c3d44ffd02115d1c67e86922853ace21b
ben <b.roullet@skilvioo.com>
2017-04-25 15:52:11 +0200

    :art: Use new userlist component for validator user list

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -22,46 +22,11 @@ import { FilterUserPipe } from '../../pipes/filteruser.pipe'
 })
 export class UsersComponent implements OnInit {
 
-  userList: Observable<User[]>;
-  private searchTerms = new Subject<string>();
-
   constructor(
-    private usersService: UsersService,
-    private filteruser: FilterUserPipe
   ) { }
 
   ngOnInit() {
-    this.userList = this.populateUserList();
 
-    this.searchTerms
-      .debounceTime(300)        // wait 300ms after each keystroke before considering the term
-      .distinctUntilChanged()   // ignore if next search term is same as previous
-      .subscribe((term) => {
-        if(term) {
-          this.userList = this.searchUser(term);
-        } else {
-          this.userList = this.populateUserList();
-        }
-      })
-  }
-
-  search(term: string): void {
-    this.searchTerms.next(term);
-  }
-
-  searchUser(term: string): Observable<User[]> {
-    return this.filteruser.transform(this.userList, term);
-  }
-
-  populateUserList(): Observable<User[]> {
-    return this.usersService.getUsers()
-      .map((users) => {
-        if (users.length > 0) {
-          return users;
-        } else {
-          return [];
-        }
-      })
   }
 
 }
